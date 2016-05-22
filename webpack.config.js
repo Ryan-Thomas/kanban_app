f72f1a8c37acd86957c4d67479cb53870b391c00
Ryan Thomas <ryanthomas970@gmail.com>
2016-05-22 11:00:54 -0700

    Hot Module Replacement Configured. Website now autoreloads Javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,58 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
-  app: paths.join('__dirname', 'app'),
-  build: paths.join('__dirname', 'build')
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-  // Entry accepts a path or an object of entries. We will be using the latter
-  // form given it's more convenient with more complex operations
+const common = {
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
   entry: {
-    app: 'PATHS.app'
+    app: PATHS.app
   },
   output: {
-    path: 'PATHS.build',
+    path: PATHS.build,
     filename: 'bundle.js'
   }
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so that HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output
+      stats: 'errors-only',
+
+      // Parse host and env so that this is easy to customize.
+      //
+      // If you use vagrant or Cloud9, set
+      // host: process.env.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.host,
+      port: process.env.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
